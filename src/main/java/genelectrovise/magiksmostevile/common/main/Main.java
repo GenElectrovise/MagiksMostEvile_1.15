58993c602a4f3ba85d8d41de8ce25c1009de468e
GenElectrovise <sanchez.pamelos@outlook.com>
2020-02-09 13:43:00 +0000

    First commit to initalise repo

---
+++ src/main/java/genelectrovise/magiksmostevile/common/main/Main.java
@@ -0,0 +1,28 @@
+package genelectrovise.magiksmostevile.common.main;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+
+//@Mod(modid = Main.MODID, name = Main.NAME, version = Main.VERSION, value = "")
+
+@Mod(Main.MODID)
+@Mod.EventBusSubscriber(modid = Main.MODID, bus = Mod.EventBusSubscriber.Bus.MOD)
+public class Main {
+	public static final Logger LOGGER = LogManager.getLogger();
+	public static final String MODID = "magiksmostevile";
+	public static final String VERSION = "1.0";
+	public static final String NAME = "MagiksMostEvile";
+	public static final String ACCEPTED_VERSIONS = "1.15.2";
+
+	//public static Main instance = new Main();
+	
+	public Main() {
+		LOGGER.info("Welcome to Magiks Most Evile! Hello from the Main class! (If you can see this message, Magiks Most Evile is being loaded by Forge! Woohoo!)");
+		Main.LOGGER.debug("Log Key 182927012020 : FMLJavaModLoadingContext = " + FMLJavaModLoadingContext.get());
+		new EvileRegistry();
+	}
+
+}
---
+++ src/main/java/genelectrovise/magiksmostevile/common/world/gen/EvileFeature.java
@@ -0,0 +1,8 @@
+package genelectrovise.magiksmostevile.common.world.gen;
+
+import net.minecraft.world.gen.feature.Feature;
+import net.minecraft.world.gen.feature.IFeatureConfig;
+
+public abstract class EvileFeature<FC extends IFeatureConfig> extends net.minecraftforge.registries.ForgeRegistryEntry<Feature<?>> {
+
+}
